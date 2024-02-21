9f85fc53a4831c03decc3fdfde7b53dc12882dca
erkan011 <erkancaliskan011@gmail.com>
2024-02-21 20:19:17 +0300

    52. Soru Cevaplandı

---
+++ Algoritma_Sorusu/src/Main.kt
@@ -0,0 +1,85 @@
+import java.util.InputMismatchException
+import java.util.Scanner
+
+fun main(){
+
+    println("Welcome to Simple Mathematical Operation Application!")
+
+    val scanner=Scanner(System.`in`)
+
+    var continueCalculation =true
+
+    while (continueCalculation){
+
+        println("Enter the first number: ")
+
+        val firstNumber = getUserInput(scanner)
+
+        println("Choose the mathematical operation you want to perform (+, -, *, /):")
+
+        val operator = getOperator(scanner)
+
+        println("Enter the second number: ")
+
+        val secondNumber = getUserInput(scanner)
+
+        try {
+            val result = calculate(firstNumber, secondNumber, operator)
+            println("Result: $result")
+
+        }catch (e: ArithmeticException){
+
+            println("Error: ${e.message}")
+        }
+        println("Do you want to continue? (Enter 'y' or 'Y' for Yes): ")
+
+        val continueInput = scanner.next()
+
+        continueCalculation = continueInput.equals("y", ignoreCase = true)
+    }
+
+    println("Application terminate. Have a nice day!")
+}
+
+fun getOperator(scanner: Scanner): Char{
+
+    while (true){
+
+        val input = scanner.next()
+        if (input.length == 1 && "+-*/".contains(input)){
+            return input[0]
+        }else{
+
+         println("Error: Invalid operation operator. Try again.")
+
+        }
+    }
+}
+
+fun calculate(firstNumber: Double, secondNumber: Double, operator: Char): Double{
+    return when(operator){
+        '+' -> firstNumber + secondNumber
+        '-' -> firstNumber - secondNumber
+        '*' -> firstNumber * secondNumber
+        '/' -> {
+            if (secondNumber == 0.0) {
+                throw ArithmeticException("Division by zero error!")
+            }else{
+                firstNumber / secondNumber
+            }
+        }
+        else -> throw IllegalArgumentException("Invalid operation operator.")
+    }
+}
+fun getUserInput(scanner: Scanner):Double{
+    while (true){
+
+        try {
+            return scanner.nextDouble()
+        }catch (e: InputMismatchException) {
+            println("Error: Please enter a number.")
+            scanner.next()
+        }
+    }
+
+}
